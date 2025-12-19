4d09a69dc4fe84a563de56d8440e1acf7229c2c6
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2025-12-19 19:06:45 -0300

    Exercicio 003 - Elementos -  Concluido

--- Exercicios Js básico/Elementos/ex003 - O Irmão Intrometido/script.js
+++ Exercicios Js básico/Elementos/ex003 - O Irmão Intrometido/script.js
@@ -1,3 +1,10 @@
+let msgErro = document.getElementsByTagName('span');
 
+
+function desfoque(e){
+    let d = e.nextElementSibling
+    d.textContent = "Preencha esse campo!"
+    d.style.color = "red"
+} 
 
 
