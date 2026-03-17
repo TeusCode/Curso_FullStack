66c55a9005c516901e437350b3f8f8feea1f7756
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2026-03-16 23:42:17 -0300

    Create script.js

---
+++ Exercicios JavaScript/JavaScriptAvancado/This e Bind/script.js
@@ -0,0 +1,3 @@
+/*This é básicamente um representante do que está como protagonista no contexto, por exemplo, se eu uso o this em uma função, o this será o elemento html que o acionou, ou seja o this depende do contexto (This em inglês significa: Isto!)*/
+
+/*Ele aponta para "quem" está executando o código naquele momento*/
